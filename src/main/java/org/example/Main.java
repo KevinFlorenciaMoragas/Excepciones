a15ad7bbf4db44ca611b6e9ee1828de14272a2a3
KevinFlorenciaMoragas <120371012+KevinFlorenciaMoragas@users.noreply.github.com>
2023-05-18 20:54:50 +0200

    18/05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,11 @@ import org.example.Ejemplo.Datos.*;
 import org.example.Ejemplo.Datos.*;
 import org.example.Ejemplo.Excepciones.AccesoDatosEx;
 import org.example.model.Division;
+import org.example.model.ExcepcionesParking;
 import org.example.model.IndeterminacionEx;
+import org.example.model.Parking;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws AccesoDatosEx {
@@ -19,11 +23,40 @@ public class Main {
         }
         System.out.println("Continuamos con el flujo del programa");
 */
+        /*
         AccesoDatos accesoDatos = new ImplementacionOracle();
         accesoDatos.simularError(false);
         ejecutar(accesoDatos, "listar");
+         */
 
-    }
+        try{
+            Parking parking = new Parking("Parking", 10);
+            parking.setArrayListSize(parking.getSize());
+            System.out.println(parking.toString());
+            ArrayList<String> matriculas = new ArrayList<>();
+            parking.entrada("1231231qweqweqw2312314", 4);
+            parking.entrada("1235", 3);
+            parking.entrada("1236", 2);
+            System.out.println(parking.toString());
+
+            parking.salida("1234");
+            System.out.println(parking.getPlazasTotales());
+            System.out.println(parking.getPlazasLibres());
+            System.out.println(parking.getPlazasOcupadas());
+            //parking.setMatriculas(matriculas);
+            System.out.println(parking.toString());
+        }catch (ExcepcionesParking ex) {
+            ex.printStackTrace(System.out);
+        }catch (Exception ex){
+            System.out.println("Error general");
+            ex.printStackTrace(System.out);
+        }
+        finally {
+            System.out.println("Procesar finally es opcional, siempre se ejecuta");
+
+        }
+
+        }
     public static void ejecutar(AccesoDatos datos,String operacion) {
         try{
             if(operacion.equals("listar")){
--- src/main/java/org/example/model/Parking.java
+++ src/main/java/org/example/model/Parking.java
@@ -5,8 +5,15 @@ import java.util.ArrayList;
 public class Parking {
     ArrayList<String> matriculas = new ArrayList<String>();
     String name;
-    public Parking(String name) {
+    int size;
+    public void setArrayListSize(int size){
+        for(int i = 0; i < size; i++){
+            matriculas.add(null);
+        }
+    }
+    public Parking(String name, int size) {
         this.name = name;
+        this.size = size;
     }
     public Parking() {
     }
@@ -26,32 +33,65 @@ public class Parking {
     public void setName(String name) {
         this.name = name;
     }
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public int getSize() {
+        return size;
+    }
     public void entrada(String matricula, int plaza) throws ExcepcionesParking {
         int matriculasSize = matriculas.size();
-        if(matriculasSize == 0 || matriculasSize < 4){
-            throw new ExcepcionesParking("El parking esta vacio");
+        int matrSize = matricula.length();
+        if(matrSize == 0 || matrSize < 4 || matrSize > 10){
+            throw new ExcepcionesParking("La matricula esta equivocada");
         }
-        if(plaza > 0){
+        if(plaza >= 0 && plaza < matriculas.size() && matriculas.get(plaza) != null){
             throw new ExcepcionesParking("La plaza esta ocupada");
         }
         if(matriculas.contains(matricula)){
             throw new ExcepcionesParking("La matricula ya esta en el parking");
         }
-        matriculas.add(matricula);
+        System.out.println("La matricula " + matricula + " ha entrado en la plaza " + plaza);
+        matriculas.set(plaza,matricula);
     }
     public int salida(String matricula) throws ExcepcionesParking {
         int matriculasSize = matriculas.size();
         if(matriculasSize == 0){
-            return 0;
+            throw new ExcepcionesParking("La matricula esta equivocada");
         }
         if(matriculas.contains(matricula)){
             matriculas.remove(matricula);
+            System.out.println("La matricula " + matricula + " ha salido del parking");
             return 1;
         }
         if(matriculas.contains(matricula) == false){
             throw new ExcepcionesParking("La matricula no esta en el parking");
         }
+        System.out.println("La matricula " + matricula + " ha salido del parking");
         return 0;
+    }
+    public int getPlazasTotales(){
+        return matriculas.size();
+    }
+    public int getPlazasLibres(){
+        return this.size - matriculas.size();
+    }
+    public int getPlazasOcupadas(){
+        return matriculas.size();
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i< matriculas.size();i++){
+            sb.append("Plaza: ").append(i+1).append(": ");
+            if(matriculas.get(i) != null){
+                sb.append(matriculas.get(i));
+            }else{
+                sb.append("Libre");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
     public void visualizar() {
         System.out.println("El parking " + name + " tiene las siguientes matriculas: " + matriculas);
